81d63ed6d33f9c80bc784f5b49f3974533a3cda1
Martinsr99 <silesreche.martin@gmail.com>
2023-08-25 14:36:58 +0200

    feat: Connect Nest && Mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..','public')
     }),
+    MongooseModule.forRoot('mongodb://localhost:27018/nest-pokemon'),
     PokemonModule
   ]
 })
