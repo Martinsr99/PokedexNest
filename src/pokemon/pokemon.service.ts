4b725ea0d2765d18eb96780e43f43de8185774a0
Martinsr99 <silesreche.martin@gmail.com>
2023-08-26 13:43:07 +0200

    feat: Add validation to update unique pokemon

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -22,11 +22,7 @@ export class PokemonService {
           return pokemon
       
     } catch (error) {
-      if(error.code === 11000) {
-        throw new BadRequestException(`Pokemon exists in DB ${JSON.stringify(error.keyValue)}`)
-      }
-      console.log(error)
-      throw new InternalServerErrorException(`Can't create Pokemon - Check server logs`)
+      this.handleExceptions(error)
     }
   }
 
@@ -59,11 +55,32 @@ export class PokemonService {
     return pokemon
   }
 
-  update(id: number, updatePokemonDto: UpdatePokemonDto) {
-    return `This action updates a #${id} pokemon`;
+  async update(term: string, updatePokemonDto: UpdatePokemonDto) {
+
+    const pokemon = await this.findOne(term)
+
+    if(updatePokemonDto.name) updatePokemonDto.name = updatePokemonDto.name.toLowerCase()
+
+    try {
+      await pokemon.updateOne(updatePokemonDto)
+  
+      return {...pokemon.toJSON(), ...updatePokemonDto}
+      
+    } catch (error) {
+      this.handleExceptions(error)
+    }
+
   }
 
   remove(id: number) {
     return `This action removes a #${id} pokemon`;
+  }
+
+  private handleExceptions (error: any) {
+    if(error.code === 11000) {
+      throw new BadRequestException(`Pokemon exists in DB ${JSON.stringify(error.keyValue)}`)
+    }
+    console.log(error)
+    throw new InternalServerErrorException(`Can't create Pokemon - Check server logs`)
   }
 }
